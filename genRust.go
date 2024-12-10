aefb0ff658408b02e1261949c2d20c2bf85ad377
Harishankar Narayanan <nharishankar@gmail.com>
2024-12-10 19:07:05 +0530

    Fixed issues for latest changes in rust project structure

--- genRust.go
+++ genRust.go
@@ -118,8 +118,7 @@ func (gen *CodeGenerator) GenRust() error {
 	var imports = `
 #![allow(unused_imports)]
 use regex::Regex;
-use crate::common::*;
-use open_payments_common::ValidationError;
+use open_payments_common::{common::*, ValidationError};
 #[cfg(feature = "derive_serde")]
 use serde::{Deserialize, Serialize};`
 	source := []byte(fmt.Sprintf("%s\n\n%s", copyright, imports+gen.Field))
